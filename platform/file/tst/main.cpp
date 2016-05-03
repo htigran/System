825f3569d565fbb1cbd10812d3ed445918e99c43
tigranza <tighovhann@gmail.com>
2016-05-03 22:16:47 +0100

    fixed file unit-test for gcc
    FileMode::FileMode -> FileMode

--- platform/file/tst/main.cpp
+++ platform/file/tst/main.cpp
@@ -17,7 +17,7 @@ namespace platform
 //////////////////////////////////////////////////////
 TEST(FileMode, Int)
 {
-	FileMode::FileMode f(1);
+	FileMode f(1);
 	EXPECT_EQ((int )f, 1);
 	EXPECT_EQ(f.getMode(), 1);
 	EXPECT_STREQ(f.getStringMode().c_str(), "--------x");
@@ -25,52 +25,52 @@ TEST(FileMode, Int)
 
 TEST(FileMode, FilePermission)
 {
-	FileMode::FileMode fxo(FileMode::XO);
+	FileMode fxo(FileMode::XO);
 	EXPECT_EQ((int )fxo, FileMode::XO);
 	EXPECT_EQ(fxo.getMode(), FileMode::XO);
 	EXPECT_STREQ(fxo.getStringMode().c_str(), "--------x");
 
-	FileMode::FileMode fro(FileMode::RO);
+	FileMode fro(FileMode::RO);
 	EXPECT_EQ((int )fro, FileMode::RO);
 	EXPECT_EQ(fro.getMode(), FileMode::RO);
 	EXPECT_STREQ(fro.getStringMode().c_str(), "------r--");
 
-	FileMode::FileMode fwo(FileMode::WO);
+	FileMode fwo(FileMode::WO);
 	EXPECT_EQ((int )fwo, FileMode::WO);
 	EXPECT_EQ(fwo.getMode(), FileMode::WO);
 	EXPECT_STREQ(fwo.getStringMode().c_str(), "-------w-");
 
-	FileMode::FileMode fxg(FileMode::XG);
+	FileMode fxg(FileMode::XG);
 	EXPECT_EQ((int )fxg, FileMode::XG);
 	EXPECT_EQ(fxg.getMode(), FileMode::XG);
 	EXPECT_STREQ(fxg.getStringMode().c_str(), "-----x---");
 
-	FileMode::FileMode frg(FileMode::RG);
+	FileMode frg(FileMode::RG);
 	EXPECT_EQ((int )frg, FileMode::RG);
 	EXPECT_EQ(frg.getMode(), FileMode::RG);
 	EXPECT_STREQ(frg.getStringMode().c_str(), "---r-----");
 
-	FileMode::FileMode fwg(FileMode::WG);
+	FileMode fwg(FileMode::WG);
 	EXPECT_EQ((int )fwg, FileMode::WG);
 	EXPECT_EQ(fwg.getMode(), FileMode::WG);
 	EXPECT_STREQ(fwg.getStringMode().c_str(), "----w----");
 
-	FileMode::FileMode fxu(FileMode::XU);
+	FileMode fxu(FileMode::XU);
 	EXPECT_EQ((int )fxu, FileMode::XU);
 	EXPECT_EQ(fxu.getMode(), FileMode::XU);
 	EXPECT_STREQ(fxu.getStringMode().c_str(), "--x------");
 
-	FileMode::FileMode fru(FileMode::RU);
+	FileMode fru(FileMode::RU);
 	EXPECT_EQ((int )fru, FileMode::RU);
 	EXPECT_EQ(fru.getMode(), FileMode::RU);
 	EXPECT_STREQ(fru.getStringMode().c_str(), "r--------");
 
-	FileMode::FileMode fwu(FileMode::WU);
+	FileMode fwu(FileMode::WU);
 	EXPECT_EQ((int )fwu, FileMode::WU);
 	EXPECT_EQ(fwu.getMode(), FileMode::WU);
 	EXPECT_STREQ(fwu.getStringMode().c_str(), "-w-------");
 
-	FileMode::FileMode fwN(FileMode::None);
+	FileMode fwN(FileMode::None);
 	EXPECT_EQ((int )fwN, FileMode::None);
 	EXPECT_EQ(fwN.getMode(), FileMode::None);
 	EXPECT_STREQ(fwN.getStringMode().c_str(), "---------");
@@ -79,7 +79,7 @@ TEST(FileMode, FilePermission)
 	| FileMode::RG | FileMode::XG | FileMode::WU | FileMode::RU
 	| FileMode::XU;
 
-	FileMode::FileMode fwA(all);
+	FileMode fwA(all);
 	EXPECT_EQ((int )fwA, all);
 	EXPECT_EQ(fwA.getMode(), all);
 	EXPECT_STREQ(fwA.getStringMode().c_str(), "rwxrwxrwx");
