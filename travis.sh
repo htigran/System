d07f20f310b3b319ccfbef0fb4c49d723a6d9138
tigranza <tighovhann@gmail.com>
2016-05-04 13:28:54 +0100

    fix for MacOS build for travis

--- travis.sh
+++ travis.sh
@@ -3,6 +3,8 @@
 set -evx
 env | sort
 
+cd /usr/src/gtest && sudo cmake . && sudo cmake --build . && sudo mv libg* /usr/local/lib/ ; cd -
+
 mkdir build || true
 cd build
 cmake -DCMAKE_CXX_FLAGS=$CXX_FLAGS \
